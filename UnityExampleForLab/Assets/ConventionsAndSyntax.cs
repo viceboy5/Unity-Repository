7a9a1541ace55f24adbe3c1ede882c281230bc2f
Hank Weiss <10811528@my.uvu.edu>
2022-06-24 22:17:23 -0600

    Copying AnotherClass into Unity Script

--- UnityExampleForLab/Assets/ConventionsAndSyntax.cs
+++ UnityExampleForLab/Assets/ConventionsAndSyntax.cs
@@ -1,18 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class ConventionsAndSyntax : MonoBehaviour
+public class ConentionsAndSyntax : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        //this line is there to tell me the x position of my object
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        /*Hi there!
+         * this is two lines!
+         * */
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
     }
 }
