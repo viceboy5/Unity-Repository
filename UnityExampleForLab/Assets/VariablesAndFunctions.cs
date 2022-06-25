303f0afa312c78b00c41020686cdc54d4f3f5820
Hank Weiss <10811528@my.uvu.edu>
2022-06-24 22:12:31 -0600

    Moved from different Unity Project

--- UnityExampleForLab/Assets/VariablesAndFunctions.cs
+++ UnityExampleForLab/Assets/VariablesAndFunctions.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class VariablesAndFunctions : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int myInt = 5;
+
+
     void Start()
     {
-        
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    int MultiplyByTwo(int number)
     {
-        
+        int result;
+        result = number * 2;
+        return result;
     }
 }
