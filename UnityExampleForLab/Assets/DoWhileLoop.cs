4b333f939134842d5074b26c2ad0de1b1e4ecf48
Hank Weiss <10811528@my.uvu.edu>
2022-06-24 22:21:00 -0600

    Added ForLoop to Unity Scripts

--- UnityExampleForLab/Assets/DoWhileLoop.cs
+++ UnityExampleForLab/Assets/DoWhileLoop.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DoWhileLoop : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        bool shouldContinue = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DoWhileLoop
+        {
+            print("Hello World");
+        }
+        while (shouldContinue == true) ;
     }
 }
