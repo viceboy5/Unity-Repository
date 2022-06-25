ca7718ff94b29d959b35ee40915f2cd6cb209f6f
Hank Weiss <10811528@my.uvu.edu>
2022-06-24 22:14:04 -0600

    Copied Over from Other Unity Project, scope and access modifiers

--- UnityExampleForLab/Assets/BehaviourScript.cs
+++ UnityExampleForLab/Assets/BehaviourScript.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
     }
 }
