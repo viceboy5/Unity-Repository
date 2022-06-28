1a359efb7cd18a8603ead8236e4ada22e3ccfb76
Hank Weiss <10811528@my.uvu.edu>
2022-06-27 20:21:54 -0600

    Created Update/FixedUpdate Scripts

--- UnityExampleForLab/Assets/UpdateAndFixedUpdate.cs
+++ UnityExampleForLab/Assets/UpdateAndFixedUpdate.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AwakeAndStart : MonoBehaviour
+public class UpdateAndFixedUpdate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-        //Set Ammo for the enemy
     {
-        Debug.Log("Awake called.");
+        
     }
 
     // Update is called once per frame
-    void Start()
-        //Allow enemy to Shoot
+    void Update()
     {
-        Debug.Log("Start called.");
+        
     }
 }
