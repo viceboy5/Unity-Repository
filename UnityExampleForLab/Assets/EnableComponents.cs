85dd70bdda3acaaea2b67f795b88504d1e889dc6
Hank Weiss <10811528@my.uvu.edu>
2022-06-30 18:48:50 -0600

    Closed out Unity Scripts, created Unit 4 project, FloatData script and Health Object

--- UnityExampleForLab/Assets/EnableComponents.cs
+++ UnityExampleForLab/Assets/EnableComponents.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnableComponents : MonoBehaviour
 {
+    private Light myLight;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myLight = GetComponent<Light>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            myLight.enabled = !myLight.enabled;
+        }
     }
 }
