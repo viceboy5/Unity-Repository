a1f3fb626750e400c784c5964a72fb3ff35ef3db
Hank Weiss <10811528@my.uvu.edu>
2022-07-05 18:22:47 -0600

    Created folder for Unit5 and copying Event project over.

---
+++ Unit5/LabScriptableObjects/Assets/Vector3Data.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vector3Data : MonoBehaviour
+{
+    public Vector3 objectPosition = new Vector3(3, 3, 3);
+    public Vector3 playerPostition = new Vector3(4,4,4);
+}
