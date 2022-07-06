8c9fcb263244baca99860a609157a77bd2600da6
Hank Weiss <10811528@my.uvu.edu>
2022-07-05 18:27:02 -0600

    Copying Scriptable Objects to Unit 5

---
+++ Unit5/ScriptableObjectsTutorial/Assets/Scenes/FloatData.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class FloatData : ScriptableObject
+{
+    public float value;
+
+    public void UpdateValue (float num)
+    {
+        value += num;
+    }
+}
