b90d7d26d4ac820c8c672f1998633f5da10dcbb3
Hank Weiss <10811528@my.uvu.edu>
2022-07-06 21:39:57 -0600

    Added RemoveText event to Discover Object script.
    text now removes when mouse lets up

--- Unit4/Lab5UnityEvents/Assets/Scenes/DiscoverObject.cs
+++ Unit4/Lab5UnityEvents/Assets/Scenes/DiscoverObject.cs
@@ -4,10 +4,15 @@ using UnityEngine.Events;
 
 public class DiscoverObject : MonoBehaviour
 {
-    public UnityEvent nameObject;
+    public UnityEvent nameObject, removeText;
 
     private void OnMouseDown()
     {
         nameObject.Invoke();
+    }
+
+    private void OnMouseUp()
+    {
+        removeText.Invoke();
     }
 }
