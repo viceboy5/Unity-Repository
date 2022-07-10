af5faae6e97d409d7ec740d97da019554a58bd16
Hank Weiss <10811528@my.uvu.edu>
2022-07-10 17:12:00 -0600

    Created Projectile, coded movement.  coded destroy for Laster and colliders with player

--- UFO Defense Force/Assets/Scripts/PlayerController.cs
+++ UFO Defense Force/Assets/Scripts/PlayerController.cs
@@ -8,12 +8,10 @@ public class PlayerController : MonoBehaviour
     public float speed = 25;
     public float xRange = 40;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform blaster;
+    public GameObject lazerBolt;
 
+  
     // Update is called once per frame
     void Update()
     {
@@ -34,7 +32,17 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+        // if spacebar is pressed, fire lazerbolt
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //Creates lazerBolt at the blaster transform position maintaining the object's rotation
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
 
-
+    }
+    // Delete any object with a trigger that hits the player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
     }
 }
