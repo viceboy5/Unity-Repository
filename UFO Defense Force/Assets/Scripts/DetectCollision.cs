b668dbc6810f0e53869e50d3dd5975186e5b3ace
Hank Weiss <10811528@my.uvu.edu>
2022-07-21 17:55:36 -0600

    Added Main Menu UI, Added Score System Scripts

--- UFO Defense Force/Assets/Scripts/DetectCollision.cs
+++ UFO Defense Force/Assets/Scripts/DetectCollision.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class DetectCollision : MonoBehaviour
 {
-     void OnTriggerEnter(Collider other)
+
+    public ScoreManager scoreManager;  // Store reference to score manager
+
+
+    public int scoreToGive;
+
+    private void Start()
     {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>(); // Find ScoreManager gameobject and reference ScoreManager script component
+    }
+
+
+    void OnTriggerEnter(Collider other)  // Once the Trigger has been entered record collision in the argument variable "other"
+    {
+        scoreManager.IncreaseScore(scoreToGive); 
         Destroy(gameObject); 
         Destroy(other.gameObject);
     }
