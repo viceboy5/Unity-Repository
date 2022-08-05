12fc3ecaec22362beb2dec294e2bc455c379873c
Hank Weiss <10811528@my.uvu.edu>
2022-08-04 20:08:35 -0600

    Created new Scene, using Coroutines for text changes

---
+++ Color Game/Color Game/Assets/Scripts/CoroutineBehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineBehaviour : MonoBehaviour
+{
+
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public IntData counterNum;
+
+    public float seconds = 3.0f;
+    private WaitForSeconds wfsObj; 
+    private WaitForFixedUpdate wffuObj;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        wffuObj = new WaitForFixedUpdate();
+        startEvent.Invoke();
+        yield return wfsObj;
+        while (counterNum.value > 0)
+        {
+            repeatEvent.Invoke();
+            counterNum.value--;
+            yield return wfsObj;
+        }    
+        endEvent.Invoke();
+
+
+    }
+}
