ada1ac353ed63c6d774323ee14b42116e54ffcc0
Hank Weiss <10811528@my.uvu.edu>
2023-05-17 19:04:57 -0600

    removing unused projects

--- Unit4/LabScriptableObjects/Assets/Vector3Data.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Vector3Data : MonoBehaviour
-{
-    public Vector3 objectPosition = new Vector3(3, 3, 3);
-    public Vector3 playerPostition = new Vector3(4,4,4);
-}
